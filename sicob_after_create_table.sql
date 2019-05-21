4e9265e9fe1185f545d85ae9924c71a2d380fa80
Erik Vargas <erik.vargas@yandex.com>
2019-05-21 12:10:10 -0400

    Se invalido el anterior commit al comprobarse que ya existia sicob_tr_after_create_table.sql

--- sicob_after_create_table.sql
+++ sicob_after_create_table.sql
@@ -33,7 +33,3 @@ BEGIN
     END IF;
 END;
 $function$
-;CREATE EVENT TRIGGER sicob_tr_after_create_table
-ON ddl_command_end
-WHEN tag IN ('CREATE TABLE', 'ALTER TABLE')
-EXECUTE PROCEDURE public.sicob_after_create_table();
